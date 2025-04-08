217abf0fce0d7855977536286d0b3355638599eb
Francesco <you@example.com>
2025-04-08 12:20:38 +0200

    Add milestone 5

--- src/main.ts
+++ src/main.ts
@@ -87,7 +87,7 @@ function isActress(data: unknown): data is Actress {
   return false;
 }
 
-async function getActress(id: number): Promise<object | null> {
+async function getActress(id: number): Promise<Actress | null> {
   try {
     const response = await fetch(
       `https://boolean-spec-frontend.vercel.app/freetestapi/actresses/${id}`
@@ -151,3 +151,28 @@ async function getAllActresses(): Promise<object | (number | string)[]> {
     .catch((error) => console.error(error));
   return result;
 })();
+
+//! Milestone 5
+
+/* Crea una funzione getActresses che riceve un array di numeri (gli id delle attrici).
+
+Per ogni id nell’array, usa la funzione getActress che hai creato nella Milestone 3 per recuperare l’attrice corrispondente.
+
+L'obiettivo è ottenere una lista di risultati in parallelo, quindi dovrai usare Promise.all.
+
+La funzione deve restituire un array contenente elementi di tipo Actress oppure null (se l’attrice non è stata trovata). */
+
+async function getActresses(ids: number[]): Promise<(Actress | null)[]> {
+  try {
+    const promises = ids.map((id) => getActress(id));
+    const actresses = await Promise.all(promises);
+    return actresses;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+getActresses([3, 4, 5])
+  .then((data) => console.log(data))
+  .catch((error) => console.error(error));
